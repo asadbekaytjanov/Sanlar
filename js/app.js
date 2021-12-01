05de75629f05e6db0151a3fc6b92313e22da9544
asadbekaytjanov <asadbekaytjanov@gmail.com>
2021-12-01 14:28:40 +0500

    message

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
- function start(brain, check){
-     if(document.getElementById("st").onclick===i){
-          console.log("I'ts working!")
-          document.getElementById("ozg").innerHTML="Oyın Baslandı, men oylaǵan sandı kiritiń!"
-          brain += Math.random()*50+1; }
-          if(document.getElementById("check").onclick.j===1){
-                  if(document.getElementById("ip")===brain){
-              document.getElementById("ozg").innerHTML="Duris, men oylaǵan san "+brain+" edi!";
-    }
-    else{ if(document.getElementById("ip")!==brain)
-              document.getElementById("ozg").innerHTML="Qate men oylaǵan san "+brain+ " edi, qaytadan urınıp kórıń";
-    }
-
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+  });
+  function Game() {
+    document.getElementById("ozg").innerHTML="Oyın Baslandı, men 1 den 50 ge shekem san oyladım";
+    var brain = Math.floor(Math.random()*50)+1; 
+return brain;
 }
-
-
+     function  checkGame(){
+     var brain = this.Game()
+    if(document.getElementById("ip")==brain){
+        console.log(getElementById("ozg").innerHTML="Durıs, men oylaǵan san  "+brain+" edi !!!");
+    }else{
+        if(document.getElementById("ip")!==brain){
+          document.getElementById("ozg").innerHTML="Qate, men oylaǵan san "+brain+" edi. Qaytadan oynap kóriń";
+    }
+  
+    
+ }
 }
- 
